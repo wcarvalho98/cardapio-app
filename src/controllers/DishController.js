c6bafb9a5d730252249a6720acf698ea63e1774f
Wilder Carvalho <wcarvalho98@gmail.com>
2023-08-24 17:29:59 -0300

    feat(dish): correction of dishes

--- src/controllers/DishController.js
+++ src/controllers/DishController.js
@@ -1,41 +1,41 @@
-const express = require('express');
-const router = express.Router();
-const Dish = require('../models/Dish');
-const Joi = require('joi');
-const jwt = require('../config/midleware');
+const express = require("express")
+const router = express.Router()
+const Dish = require("../models/Dish")
+const Joi = require("joi")
+const jwt = require("../config/midleware")
 
 const DishSchema = Joi.object({
-    name: Joi.string().required(),
-    price: Joi.string().required(),
-    amount: Joi.number().required(),
-    ImageUrl: Joi.string().required(),
-    Decription: Joi.string().required(),
-    Weigth: Joi.string().required(),
-    PeopleServed: Joi.number().required(),
-    Category: Joi.string().required(),
-});
+  name: Joi.string().required(),
+  price: Joi.string().required(),
+  amount: Joi.number().optional(),
+  imageUrl: Joi.string().optional(),
+  description: Joi.string().required(),
+  weight: Joi.string().optional(),
+  peopleServed: Joi.number().optional(),
+  category: Joi.string().required(),
+})
 
-router.post('/', jwt, async (req, res) => {
-    const { error } = DishSchema.validate(req.body);
+router.post("/", jwt, async (req, res) => {
+  const { error } = DishSchema.validate(req.body)
 
-    if (error) {
-        return res.status(400).json({ error: error.details[0].message });
-    }
-    try {
-        const dish = await Dish.create(req.body);
-        return res.status(201).json(dish);
-    } catch (error) {
-        return res.status(500).send('Erro ao criar prato');
-    }
-});
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message })
+  }
+  try {
+    const dish = await Dish.create(req.body)
+    return res.status(201).json(dish)
+  } catch (error) {
+    return res.status(500).send("Erro ao criar prato")
+  }
+})
 
-router.get('/:id', async (req, res) => {
-    try {
-        const dish = await Dish.findById(req.params.id);
-        return res.status(200).json(dish);
-    } catch (error) {
-        return res.status(500).send('Erro ao procurar o prato');
-    }
-});
+router.get("/:id", async (req, res) => {
+  try {
+    const dish = await Dish.findById(req.params.id)
+    return res.status(200).json(dish)
+  } catch (error) {
+    return res.status(500).send("Erro ao procurar o prato")
+  }
+})
 
-module.exports = router;
+module.exports = router
--- src/models/Dish.js
+++ src/models/Dish.js
@@ -1,15 +1,15 @@
 const mongoose = require("../config/database")
 
 const DishScheme = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: String, required: true },
-    amount: { type: Number, required: true },
-    ImageUrl: { type: String, required: true },
-    Decription: { type: String, required: true },
-    Weigth: { type: String, required: true },
-    PeopleServed: { type: Number, required: true },
-    Category: { type: String, required: true },
- })
+  name: { type: String, required: true },
+  price: { type: String, required: true },
+  amount: { type: Number },
+  imageUrl: { type: String },
+  description: { type: String, required: true },
+  weigth: { type: String },
+  peopleServed: { type: Number },
+  category: { type: String, required: true },
+})
 
 const Dish = mongoose.model("Dish", DishScheme)
 
